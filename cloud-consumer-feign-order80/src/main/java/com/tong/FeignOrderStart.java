bf40b6542d67db6e06c188690076c073df708e1e
tongchunpan <1809872737@qq.com>
2022-01-13 10:08:09 +0800

    master测试冲突

--- cloud-consumer-feign-order80/src/main/java/com/tong/FeignOrderStart.java
+++ cloud-consumer-feign-order80/src/main/java/com/tong/FeignOrderStart.java
@@ -17,6 +17,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 public class FeignOrderStart {
     public static void main(String[] args) {
         System.out.println("这是我使用Dev分支");
+        System.out.println("master测试冲突");
         SpringApplication.run(FeignOrderStart.class,args);
     }
 }
