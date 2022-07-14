2c95a3e8c2813ac98461f8c0c6fc0ae3d6206ea5
tongchunpan <1809872737@qq.com>
2022-07-14 09:31:31 +0800

    test

--- cloud-provider-payment8004/src/main/java/com/tong/ZookeeperStart.java
+++ cloud-provider-payment8004/src/main/java/com/tong/ZookeeperStart.java
@@ -13,5 +13,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class ZookeeperStart {
     public static void main(String[] args) {
         SpringApplication.run(ZookeeperStart.class,args);
+        System.out.println("hello test");
     }
 }
